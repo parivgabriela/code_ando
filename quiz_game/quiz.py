eb69ef84f19912a8507c2df3586b915ebce0a59d
parivgabriela <pari.gabriela@gmail.com>
2023-05-30 01:29:26 -0300

    add file with the questions and print in the file #todo input working

--- quiz_game/quiz.py
+++ quiz_game/quiz.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import csv
+
 topics = ["Programming", "Maths", "Capitals"]
+with open('questions.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        print(f'categories {row["categories"]}, questions {row["questions"]}')
 #todo build a der with the relation betbeen the data
 print("Welcome to the Quiz!")
 print("Select your topic:")
