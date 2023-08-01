234a80e2f3b79c0c0a6fafac866912cebbcdc4a9
parivgabriela <pari.gabriela@gmail.com>
2023-07-31 23:06:41 -0300

    add basic view to get a file gui and input with a button

--- pick_raffle/pick_raffle_gui.py
+++ pick_raffle/pick_raffle_gui.py
@@ -1,65 +1,38 @@
-from tkinter import * 
+import tkinter as tk
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
+from pick_raffle import select_random_number
 
 root = Tk()
-root.geometry("300x150")
-frame = Frame(root)
+root.geometry("600x400")
+frame = tk.Frame(root)
 frame.pack()
+filename = ''
 
-def ask_file_view():
-    
-    w = Label(root, text ='Pick Raffle', font = "50") 
-    w.pack()
-    
-    frame = Frame(root)
-    frame.pack()
-    
-    bottomframe = Frame(root)
-    bottomframe.pack( side = BOTTOM )
-    root.mainloop()
+def raffle():
+    cant_winners = var_cant_winners.get()
+    print("cant", cant_winners)
+    print("file", filename)
 
-    filelocation = askopenfilename(
-        filetypes=[('PDF','*.pdf')], title="Select a pdf file") # open the dialog GUI
-    return filelocation
+def select_file():
+    filename = askopenfilename(filetypes=[('CSV','*.csv'),('excel','*.xlsx')], title="Select a file")
+    print("filename", filename)
+    return filename    
 
-def demo():
-    # example
-    w = Label(root, text ='GeeksForGeeks', font = "50") 
-    w.pack()
-    
+##### body #####
+# title of window
+root.title("Pick a raffle")
+#label title
+# LABEL DE INPUT
+#label = tk.Label()
+var_cant_winners = tk.StringVar()
+entry = tk.Entry(root, textvar=var_cant_winners, width=22, relief="flat")
+entry.place(x=270, y=190)
 
-    
-    bottomframe = Frame(root)
-    bottomframe.pack( side = BOTTOM )
-    
-    b1_button = Button(frame, text ="Geeks1", fg ="red")
-    b1_button.pack( side = LEFT)
- 
-def printInput(inputtxt,lbl):
-	inp = inputtxt.get(1.0, "end-1c")
-	lbl.config(text = "Provided Input: "+inp)
-
-def view_input():
-     # TextBox Creation
-    inputtxt = Text(frame,
-    				height = 5,
-    				width = 20)
-
-    inputtxt.pack()
-    lbl = Label(frame, text = "")
-    # Button Creation
-    printButton = Button(frame,
-    						text = "Print",
-    						command = printInput(inputtxt,lbl))
-    printButton.pack()
-    lbl.pack()
-
-    frame.mainloop()
-
-
-
-
-
-
-view_input()
+# ingrese archivo
+button_file = tk.Button(root, text="Choose a file", cursor="hand2", command=select_file)
+button_file.place(x=270, y=220)
+# boton de sortear
+button_raffle = tk.Button(root, text="Raffle", cursor="hand2", command=raffle)
+button_raffle.place(x=290, y=350)
+frame.mainloop()
