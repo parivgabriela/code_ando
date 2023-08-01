2332072fe75f4973368be14136d53c8694e65cb5
parivgabriela <pari.gabriela@gmail.com>
2023-08-01 11:13:33 -0300

    add doc to the functions

--- pick_raffle/pick_raffle.py
+++ pick_raffle/pick_raffle.py
@@ -2,17 +2,17 @@ import random
 from random import randrange
 from validators import validate_number
 
-def read_file(path_file):
-    with open(path_file) as fl:
-        next(fl)
-        list_file = fl.readlines()
-    return list_file
-
-def select_random_element_list(a_list):
-    element = random.choice(a_list)
-    print(f"elemento elegido {element}")
 
 def len_file(path):
+    """return the len of a file this don't useses all memory
+    read line by line and count it
+
+    Args:
+        path (str): given a path to read the file
+
+    Returns:
+        int: return the len of the given file
+    """
     lines = 0
     with open(path) as f:
         for line in f:
@@ -21,6 +21,15 @@ def len_file(path):
     return lines
 
 def select_winners_from_file(filename, cant_winners):
+    """from a file and the numbers or winners this return the names of winners in a list
+
+    Args:
+        filename (str): path to the file of the names
+        cant_winners (int): number of winners
+
+    Returns:
+        list: return in order the winners
+    """
     number_winners = select_n_winners_from(cant_winners, len_file(filename)-1)
     winners = []
     data_winner = {}
@@ -38,11 +47,27 @@ def select_winners_from_file(filename, cant_winners):
     return winners
 
 def select_random_number(max):
-    #
+    """given a max return the random number
+
+    Args:
+        max (int): max number of the random
+
+    Returns:
+        int: random number
+    """
     rand_number = randrange(max)
     return rand_number
 
 def select_n_winners_from(n, max):
+    """given a cant of numbers and the max of a number return a list with the winner positions
+
+    Args:
+        n (int): cant of winners
+        max (int): max of a number
+
+    Returns:
+        list: list of the winner numbers
+    """
     list_winners = []
     index = 0
     while len(list_winners) != n:
@@ -53,6 +78,15 @@ def select_n_winners_from(n, max):
     return list_winners
 
 def select_random_from_list(list_file, cant_winners):
+    """return list of winner in a file
+
+    Args:
+        list_file (list): list with the participants
+        cant_winners (int): cant of winners
+
+    Returns:
+        list: list of names winners
+    """
     list_winners = []
     list_numbers = select_n_winners_from(cant_winners,len(list_file))
     for winner in range(cant_winners):
@@ -60,17 +94,9 @@ def select_random_from_list(list_file, cant_winners):
 
     return list_winners
 
-def main_random():
-    filename = input("Enter filename: ") #"names.csv"
-    list_participants = read_file(filename)
-    cant_winners = validate_number("Cant of winners: ", len(list_participants))
-    list_winners = select_random_from_list(list_participants, cant_winners)
-    print("Winners\n")
-    for index, winner in enumerate(list_winners, start=1):
-        print(f"{index} Place: {winner}\n")
-    #print(f"{} Place: {list_winners[0]}\nSecond Place: {list_winners[1]}\nThrid Place: {winner_3}")
 
 def main_random_file():
+    # main program to test in terminal
     filename = input("Enter filename: ") #"names.csv"
     cant_winners = validate_number("Cant of winners: ", len_file(filename))
     list_winners = select_winners_from_file(filename, cant_winners)
