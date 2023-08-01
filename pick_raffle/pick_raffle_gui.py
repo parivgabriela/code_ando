27e72bb6de080c0f723518b091bb1b62d43c6d40
parivgabriela <pari.gabriela@gmail.com>
2023-08-01 12:34:00 -0300

    add window to show the winners,add button to close window and return to main screen, add validations in input of cant of winners

--- pick_raffle/pick_raffle_gui.py
+++ pick_raffle/pick_raffle_gui.py
@@ -3,6 +3,7 @@ from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 from pick_raffle import select_winners_from_file
 from tkinter.messagebox import showinfo
+from validators import validate_number_input
 
 root = Tk()
 root.geometry("600x320")
@@ -10,14 +11,52 @@ frame = tk.Frame(root)
 root.resizable(False, False)
 frame.pack()
 filename = ''
+gui_title = "Pick a raffle"
+
+def close_window():
+    root.destroy()
+
+def show_winners(list_winners):
+    root.withdraw()
+    window_winner = tk.Toplevel()
+    window_winner.geometry("600x400")
+    window_winner.title(gui_title)
+    window_winner.resizable(False, False)
+
+    def return_main_window():
+        window_winner.withdraw()
+        root.deiconify()
+
+    button_return_root_window = tk.Button(window_winner, text="Return", cursor="hand2", command=return_main_window)
+    button_return_root_window.place(x=230, y=280)
+    button_close_window = tk.Button(window_winner, text="Close", cursor="hand2", command=close_window)
+    button_close_window.place(x=310, y=280)
+
+    position_y = 20
+    for index, winner in enumerate(list_winners, start=1):
+        #name_label = f"label_winner{index}"
+        position_y = position_y + 30
+        text_label = f"{index} place {winner}"
+        name_label = tk.Label(window_winner, text = text_label).place(x=220, y=position_y)
+    
+    window_winner.mainloop()
+
 
 def raffle():
     """button triger the raffle
     """
-    cant_winners = int(var_cant_winners.get()) # todo validate only numbers and must be less than the len of file
-    filename = in_filename.get()
-    list_winners = select_winners_from_file(filename, cant_winners)
-    print(list_winners)
+    try:
+        cant_winners = var_cant_winners.get() # todo validate only numbers and must be less than the len of file
+        message = validate_number_input(cant_winners)
+        if not message:
+            filename = in_filename.get()
+            list_winners = select_winners_from_file(filename, cant_winners)
+            show_winners(list_winners)
+        else:
+            showinfo(title="retry", message=message)
+    except:
+        showinfo(title="retry", message="Enter a valid number")
+    
 
 def select_file():
     """button trigger pick a file
@@ -34,7 +73,7 @@ def select_file():
 
 ##### body #####
 # title of window
-root.title("Pick a raffle")
+root.title(gui_title)
 #label title
 l_body = tk.Label(root, text = "Welcome to Pick Raffle").place(x = 220, y = 20) 
 
