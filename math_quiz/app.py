0fb1c4e76fc67f24596ce78c62f57e99ab064737
parivgabriela <pari.gabriela@gmail.com>
2024-08-12 19:19:38 -0300

    add initial structure of a math quiz

---
+++ math_quiz/app.py
@@ -0,0 +1,30 @@
+import random
+import operator
+from flask import render_template
+
+LEVEL_0 = 10
+LEVEL_1 = 100
+LEVEL_2 = 1000
+
+def generate_function(level):
+    list_operation = {'+':operator.add, 
+                      '-':operator.sub #, '*':operator.mul
+                      }
+    operator_symbol = random.choice(list(list_operation.keys()))
+    operator_f = list_operation[operator_symbol]
+    x = random.randrange(1,level)
+    y = random.randrange(1,level)
+    resultado = operator_f(x, y)
+    function_s = f"{x} {operator_symbol} {y} = "
+    if level == LEVEL_2:
+        z = random.randrange(1,level)
+    
+    return (function_s, resultado)
+
+@app.route('/')
+def index():
+    return render_template()
+
+
+print(generate_function(LEVEL_1))
+
