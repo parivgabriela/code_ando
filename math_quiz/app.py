ed3f770c01f28b6582603492d7e4380f5d0b5c6a
parivgabriela <pari.gabriela@gmail.com>
2024-08-16 15:18:49 -0300

    Change view after times up and decrease score

--- math_quiz/app.py
+++ math_quiz/app.py
@@ -1,20 +1,20 @@
 from flask import Flask, render_template, request
-from maths import generate_function
-import sys
+from maths import generate_function, result_ok, result_wrong
+
 
 app = Flask(__name__)
 
 result = None
 level = None
-result_wrong = 'Wrong! Try again!'
-result_ok = 'Well done!'
+
 score = 0
 
 @app.route('/', methods=['GET'])
 def index():
     #primero seleccionar nivel
     return render_template('index.html')
-    
+
+
 @app.route('/select_level', methods=['POST'])
 def select_level():
     global level
@@ -22,6 +22,7 @@ def select_level():
     global result
     random_function, result = generate_function(level)
     return render_template('math.html', function_r=random_function)
+
 
 @app.route('/check_result', methods=['POST'])
 def math():
@@ -31,7 +32,7 @@ def math():
     if result_input == result:
         score += 100
         random_function, result = generate_function(level)
-
+        
         return render_template('math.html', function_r=random_function, status_result=result_ok, score=score)
     else:
         score -= 10
@@ -39,6 +40,13 @@ def math():
 
         return render_template('math.html', function_r=random_function, status_result=result_ok, score=score)
         
+@app.route('/time_up', methods=['GET'])
+def time_up():
+    global result
+    global score
+    score -= 1
+    random_function, result = generate_function(level)
+    return render_template('math.html', function_r=random_function, score=score)
 
 if __name__ == "__main__":
     app.run(debug=True)
