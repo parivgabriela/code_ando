7c286dc2251f3bfee3a27d2daa272f91e193add8
parivgabriela <pari.gabriela@gmail.com>
2024-08-21 16:25:56 -0300

    delete timer with numbers

--- math_quiz/static/scripts.js
+++ math_quiz/static/scripts.js
@@ -1,4 +1,4 @@
-function startTimer(duration, display) {
+function startTimer(duration, progressBar) {
     var timer = duration, minutes, seconds;
     var interval = setInterval(function () {
         minutes = parseInt(timer / 60, 10);
@@ -7,14 +7,14 @@ function startTimer(duration, display) {
         minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        display.textContent = minutes + ":" + seconds;
+        // display.textContent = minutes + ":" + seconds;
 
         var percentage = ((duration - timer) / duration) * 100;
                 progressBar.style.width = 100 - percentage + "%";
         
         // Change color to red when timer reaches 10 seconds
         if (timer <= 10) {
-            display.classList.add("red");
+            //display.classList.add("red");
             progressBar.classList.add("red");
         }
 
@@ -31,8 +31,8 @@ function startTimer(duration, display) {
 
 window.onload = function () {
     var oneMinute = 60,
-        display = document.querySelector('#timer');
+        //display = document.querySelector('#timer');
         progressBar = document.querySelector('#progress-bar');
-    startTimer(oneMinute, display, progressBar);
+    startTimer(oneMinute, progressBar);
 };
 
