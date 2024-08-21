bc048e0f14d12beec6862c974145da8dedf5e3d7
parivgabriela <pari.gabriela@gmail.com>
2024-08-21 16:21:42 -0300

    Send levels as list to avoid modify all time the html

--- math_quiz/app.py
+++ math_quiz/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from maths import generate_function, result_ok, result_wrong
+from maths import generate_function, result_ok, result_wrong, levels
 
 
 app = Flask(__name__)
@@ -12,13 +12,14 @@ score = 0
 @app.route('/', methods=['GET'])
 def index():
     #primero seleccionar nivel
-    return render_template('index.html')
+    return render_template('index.html', list_levels=list(levels.keys()))
 
 
 @app.route('/select_level', methods=['POST'])
 def select_level():
     global level
-    level = int(request.form.get('level'))
+    level = request.form['level']
+    print(level)
     global result
     random_function, result = generate_function(level)
     return render_template('math.html', function_r=random_function)
@@ -33,12 +34,12 @@ def math():
         score += 100
         random_function, result = generate_function(level)
         
-        return render_template('math.html', function_r=random_function, status_result=result_ok, score=score)
+        return render_template('math.html', function_r=random_function, status_result=result_ok, score=score, level=level)
     else:
         score -= 10
         random_function, result = generate_function(level)
 
-        return render_template('math.html', function_r=random_function, status_result=result_ok, score=score)
+        return render_template('math.html', function_r=random_function, status_result=result_wrong, score=score, level=level)
         
 @app.route('/time_up', methods=['GET'])
 def time_up():
@@ -46,7 +47,7 @@ def time_up():
     global score
     score -= 1
     random_function, result = generate_function(level)
-    return render_template('math.html', function_r=random_function, score=score)
+    return render_template('math.html', function_r=random_function, score=score, level=level)
 
 if __name__ == "__main__":
     app.run(debug=True)
